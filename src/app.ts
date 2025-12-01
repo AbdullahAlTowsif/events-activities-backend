ad7d785a0196cd78c555e42875e09715d6bc8cb4
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-12-02 00:57:02 +0600

    dependencies installed and project init

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from "cookie-parser"
+import { envVars } from './app/config/env';
+import notFound from './app/middlewares/notFound';
+
+const app: Application = express();
+
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+//parser
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/api", router);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        message: "Events & Activities Server is running..",
+        environment: envVars.NODE_ENV,
+        uptime: process.uptime().toFixed(2) + " sec",
+        timeStamp: new Date().toISOString()
+    })
+});
+
+
+// app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
