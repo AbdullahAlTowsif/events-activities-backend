78cfc276035c26fa1a39c96662d01e4eb354cbab
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-12-10 23:48:06 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://events-activities-frontend.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
